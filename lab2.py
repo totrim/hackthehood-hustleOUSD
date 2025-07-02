f7b8a964b8905af00a90a9333b9b3f3ee4bbcb83
totrim <swaygod908@gmail.com>
2025-07-02 10:26:12 -0700

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -3,3 +3,5 @@ Name = "Jessie King"
 Nickname = "Jacka"
 age = 15.02
 Used_python = True
+Hobbies = ["Football" , "Video Games" , "Cooking" ,"Tiktoks"]
+Favorite_things = ["movie": "Sinners", "color": "Blue", "animals": "Dogs"]
